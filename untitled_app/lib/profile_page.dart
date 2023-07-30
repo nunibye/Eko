b5e6754bf660ab7fbee52aad4e62e8fd0a2f593e
christianknab <christiantknab@gmail.com>
2023-07-29 23:22:44 -0700

    loads more post data

--- untitled_app/lib/profile_page.dart
+++ untitled_app/lib/profile_page.dart
@@ -6,18 +6,23 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
+// TODO: the code here is very messy and long and can def be fixed, i was just playing around with the database at night to get it to load posts
+
 class Post {
   final Timestamp date;
   final List<dynamic> likes;
   final List<dynamic> comments;
+  final String username;
   final String text;
+  final String uid;
 
-  Post({
-    required this.date,
-    required this.likes,
-    required this.comments,
-    required this.text,
-  });
+  Post(
+      {required this.date,
+      required this.username,
+      required this.likes,
+      required this.comments,
+      required this.text,
+      required this.uid});
 }
 
 class ProfilePage extends StatefulWidget {
@@ -39,11 +44,25 @@ class _ProfilePageState extends State<ProfilePage> {
     _loadUserData();
   }
 
-  Future<String?> _getProfileImage() async {
+  Future<String> _getUsernameFromDocument(DocumentReference usernameRef) async {
+    final snapshot = await usernameRef.get();
+    final data =
+        snapshot.data() as Map<String, dynamic>?; // Explicitly cast to Map
+    return data?['username'] ?? ''; // Safely access the 'username' property
+  }
+
+  Future<String> _getUIDFromDocument(DocumentReference usernameRef) async {
+    final snapshot = await usernameRef.get();
+    final data =
+        snapshot.data() as Map<String, dynamic>?; // Explicitly cast to Map
+    return data?['uid'] ?? ''; // Safely access the 'username' property
+  }
+
+  Future<String?> _getProfileImage(String uid) async {
     try {
       var urlRef = FirebaseStorage.instance
           .ref()
-          .child("profile_pictures/${user!.uid}/profile.jpg");
+          .child("profile_pictures/$uid/profile.jpg");
       var imageUrl = urlRef.getDownloadURL();
       return imageUrl;
     } catch (e) {
@@ -95,20 +114,23 @@ class _ProfilePageState extends State<ProfilePage> {
           .doc(user?.uid)
           .collection('posts')
           .get();
-
       if (querySnapshot.docs.isNotEmpty) {
-        postsList = querySnapshot.docs.map((doc) {
+        postsList = await Future.wait(querySnapshot.docs.map((doc) async {
           var data = doc.data();
+          String username = await _getUsernameFromDocument(data[
+              'author']); // TODO: too many read calls, can fix later with less functions...
+          String uid = await _getUIDFromDocument(data['author']);
           return Post(
             date: data['date'] ?? '',
             likes: List.from(data['likes'] ?? []),
             comments: List.from(data['comments'] ?? []),
             text: data['text'] ?? '',
+            username: username,
+            uid: uid,
           );
-        }).toList();
+        }));
       }
     }
-
     return postsList;
   }
 
@@ -123,7 +145,7 @@ class _ProfilePageState extends State<ProfilePage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 FutureBuilder(
-                  future: _getProfileImage(),
+                  future: _getProfileImage(user!.uid),
                   builder: (context, snapshot) {
                     if (snapshot.hasData) {
                       return CircleAvatar(
@@ -131,7 +153,9 @@ class _ProfilePageState extends State<ProfilePage> {
                         backgroundImage: NetworkImage(snapshot.data!),
                       );
                     } else {
-                      return Container();
+                      return CircleAvatar(
+                        radius: MediaQuery.sizeOf(context).width * 0.13,
+                      );
                     }
                   },
                 ),
@@ -205,23 +229,23 @@ class _ProfilePageState extends State<ProfilePage> {
             future: _loadPosts(),
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
-                return CircularProgressIndicator(); // Show a loading indicator while fetching data
+                return CircularProgressIndicator.adaptive();
               } else if (snapshot.hasError || !snapshot.hasData) {
-                return Text("Error loading posts."); // Handle error case
+                return Text("Error loading posts.");
               } else {
                 // Display the posts in a ListView
                 return ListView.builder(
                   shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
+                  physics: const NeverScrollableScrollPhysics(),
                   itemCount: snapshot.data!.length,
                   itemBuilder: (context, index) {
                     final post = snapshot.data![index];
-                    return ListTile(
-                      title: Text(post.text),
-                      subtitle: Text(
-                          DateTime.parse(post.date.toDate().toString())
-                              .toString()),
-                      trailing: Text('${post.likes.length} Likes'),
+                    return TweetCard(
+                      username: post.username,
+                      content: post.text,
+                      date: post.date.toDate(),
+                      likes: post.likes.length,
+                      profileImageURL: _getProfileImage(post.uid),
                     );
                   },
                 );
@@ -233,3 +257,94 @@ class _ProfilePageState extends State<ProfilePage> {
     );
   }
 }
+
+class TweetCard extends StatelessWidget {
+  final String username;
+  final String content;
+  final DateTime date;
+  final int likes;
+  final Future<String?> profileImageURL;
+
+  TweetCard({
+    required this.username,
+    required this.content,
+    required this.date,
+    required this.likes,
+    required this.profileImageURL,
+  });
+
+    @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Display the profile picture as a CircleAvatar
+              FutureBuilder<String?>(
+                future: profileImageURL,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    // Show a loading indicator while the image URL is being fetched
+                    return CircularProgressIndicator();
+                  } else if (snapshot.hasError || !snapshot.hasData) {
+                    // Handle the error if fetching the image URL fails
+                    return CircleAvatar(
+                      radius: MediaQuery.of(context).size.width * 0.06,
+                      backgroundColor: Colors.grey,
+                    );
+                  } else {
+                    // Display the CircleAvatar with the fetched profile image URL
+                    return CircleAvatar(
+                      radius: MediaQuery.of(context).size.width * 0.06,
+                      backgroundImage: NetworkImage(snapshot.data!),
+                    );
+                  }
+                },
+              ),
+
+              const SizedBox(width: 10),
+              Expanded( // Use Expanded to allow the content to wrap
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      username,
+                      style: TextStyle(
+                        fontSize: 16,
+                        letterSpacing: 1,
+                        fontWeight: FontWeight.normal,
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                    ),
+                    const SizedBox(height: 8.0),
+                    Text(
+                      content,
+                      style: TextStyle(
+                        fontSize: 14, // Adjust the font size as needed
+                        color: Colors.white, // or any other color you want
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 16.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Icon(Icons.favorite_border),
+              Text('$likes Likes'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
