807d213f230acf8b78c466dd2c3d1ba4f3fc995a
Eric <echuang1@ucsc.edu>
2023-10-16 01:18:25 -0700

    search

--- untitled_app/lib/controllers/search_page_controller.dart
+++ untitled_app/lib/controllers/search_page_controller.dart
@@ -1,6 +1,36 @@
 import 'package:flutter/cupertino.dart';
+import 'package:algolia/algolia.dart';
 
+class SearchPageController with ChangeNotifier {
+  final TextEditingController _searchText = TextEditingController(text: "");
+  List<AlgoliaObjectSnapshot> _results = [];
+  bool _searching = false;
 
-class SearchPageController extends ChangeNotifier {
+  SearchPageController() {
+    _searchText.addListener(() {
+      _search();
+    });
+  }
 
+  _search() async {
+    _searching = true;
+    notifyListeners();
+
+    Algolia algolia = const Algolia.init(
+      applicationId: '0TWRTKJQ5W',
+      apiKey: '219a2653762d8c9871a260e303f0db73', // idk if this should be hidden or something
+    );
+
+    AlgoliaQuery query = algolia.instance.index('users');
+    query = query.query(_searchText.text);
+
+    _results = (await query.getObjects()).hits;
+
+    _searching = false;
+    notifyListeners();
+  }
+
+  TextEditingController get searchText => _searchText;
+  List<AlgoliaObjectSnapshot> get results => _results;
+  bool get searching => _searching;
 }
--- untitled_app/lib/views/search_page.dart
+++ untitled_app/lib/views/search_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:algolia/algolia.dart';
 
 import '../controllers/search_page_controller.dart';
 
@@ -12,11 +13,45 @@ class SearchPage extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => SearchPageController(),
       builder: (context, child) {
-        return const Scaffold(
-            body: Text("search"));
+        var controller = Provider.of<SearchPageController>(context);
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text("User Search"),
+          ),
+          body: Container(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                TextField(
+                  controller: controller.searchText,
+                  decoration: const InputDecoration(hintText: "Search username here..."),
+                ),
+                Expanded(
+                  child: controller.searching == true
+                      ? const Center(
+                          child: Text("Searching..."),
+                        )
+                      : controller.results.isEmpty
+                          ? const Center(
+                              child: Text("No results found."),
+                            )
+                          : ListView.builder(
+                              itemCount: controller.results.length,
+                              itemBuilder: (BuildContext ctx, int index) {
+                                AlgoliaObjectSnapshot snap = controller.results[index]; // idk if this should be here 
+
+                                return ListTile(
+                                  title: Text(snap.data["username"]),
+                                );
+                              },
+                            ),
+                ),
+              ],
+            ),
+          ), 
+        );
       },
     );
   }
 }
-
-
