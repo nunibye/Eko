e3a26e915903c32da45ec4d126f611c56b76e403
nunibye <38873297+nunibye@users.noreply.github.com>
2023-10-18 20:43:14 -0700

    search prints to cl, needs restructure

--- untitled_app/lib/controllers/search_page_controller.dart
+++ untitled_app/lib/controllers/search_page_controller.dart
@@ -1,34 +1,94 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../secrets/secrets.dart';
-import 'package:algolia_helper_flutter/algolia_helper_flutter.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+//import 'package:algolia_helper_flutter/algolia_helper_flutter.dart';
+
+// class SearchMetadata {
+//   final int nbHits;
+
+//   const SearchMetadata(this.nbHits);
+
+//   factory SearchMetadata.fromResponse(SearchResponse response) =>
+//       SearchMetadata(response.nbHits);
+// }
+
+class Item {
+  final String username;
+  final String uid;
+ final String firstName;
+   final String lastName;
+   final String profilePicture;
+  //this.firstName, this.lastName, this.profilePicture
+
+  Item(this.username, this.uid, this.firstName, this.lastName, this.profilePicture);
+
+  static Item fromJson(Map<String, dynamic> json) {
+    return Item(json['username'], json['uid'], json['name']['firstName'], json['name']['lastName'], json['profileData']['profilePicture']);//json['name']['firstName'], json['profileData']['profilePicture']
+  }
+}
 
 class SearchPageController extends ChangeNotifier {
   final searchTextController = TextEditingController();
 
-  final searcher = HitsSearcher(
-    applicationID: Secrets.ALGOLIA_APP_ID,
-    apiKey: Secrets.SEARCH_API_KEY,
-    indexName: 'users',
-  );
-
+  
   bool isLoading = false;
 
-  List<Map<String, dynamic>> hits = [];
-  SearchPageController() {
-    searchTextController.addListener(onSearchTextChanged);
-    searcher.responses.listen((response) {
-      if (response.hits.isEmpty) {
-        print('No hits found for search query.');
-      } else {
-        hits = response.hits;
-      }
-      isLoading = false;
-      notifyListeners();
-    },);
-  }
+  List<Item> hits = [];
 
-  void onSearchTextChanged() {
-    searcher.query(searchTextController.text);
+
+
+  void onSearchTextChanged(String s) async {
+    if(s != ''){final response = await http.post(
+      Uri.parse(
+          'https://${Secrets.ALGOLIA_APP_ID}-dsn.algolia.net/1/indexes/users/query'),
+      headers: <String, String>{
+        'X-Algolia-API-Key': Secrets.SEARCH_API_KEY,
+        'X-Algolia-Application-Id': Secrets.ALGOLIA_APP_ID,
+      },
+      body: jsonEncode(<String, String>{
+        'params': 'query=$s&hitsPerPage=10',
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      hits = (json.decode(response.body)['hits']as List)
+        .map((i) => Item.fromJson(i))
+        .toList();
+
+    for (var item in hits) {
+      print('username: ${item.username}, uid: ${item.uid}, f:${item.firstName}, l:${item.lastName}, pp:${item.profilePicture}');//, L:${item.lastName}, pp:${item.profilePicture}
+    }
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }}
+
+    
+
+    //print("test");
+    //searcher.query(s);
+    // //int i = await searcher.responses.length;
+    // await for (final value in searcher.responses) {
+    //   print(value.hits);
+    // }
+
+    // if (await searcher.responses.length != 0) {
+    //   SearchResponse? response = searcher.lastResponse;
+    //   if (response != null) {
+    //     hits = response.hits.map((e) => e['uid']).toList();
+    //     print("test");
+    //     if (hits.isNotEmpty) {
+    //       print(hits[0]);
+    //     } else {
+    //       print("no user");
+    //     }
+    //   }
+    // }
+
+    //  hits = searcher.responses
+    //     .map((event) => event.hits.map(Item.fromJson).toList());
+    // print(hits);
   }
 }
--- untitled_app/lib/views/search_page.dart
+++ untitled_app/lib/views/search_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../controllers/search_page_controller.dart';
-
+import 'package:algolia_helper_flutter/algolia_helper_flutter.dart';
 
 class SearchPage extends StatelessWidget {
   const SearchPage({Key? key}) : super(key: key);
@@ -11,7 +11,6 @@ class SearchPage extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => SearchPageController(),
       builder: (context, child) {
-        
         return Scaffold(
           appBar: AppBar(
             title: const Text("User Search"),
@@ -22,33 +21,69 @@ class SearchPage extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 TextField(
-                  controller: Provider.of<SearchPageController>(context, listen: false).searchTextController,
+                  onChanged: (s) =>
+                      Provider.of<SearchPageController>(context, listen: false)
+                          .onSearchTextChanged(s),
+                  controller:
+                      Provider.of<SearchPageController>(context, listen: false)
+                          .searchTextController,
                   decoration: const InputDecoration(
                       hintText: "Search username here..."),
                 ),
-                Expanded(
-                  child: Provider.of<SearchPageController>(context, listen: true).isLoading
-                      ? const Center(
-                          child: CircularProgressIndicator(),
-                        )
-                      : Provider.of<SearchPageController>(context, listen: true).hits.isEmpty
-                          ? const Center(
-                              child: Text("No results found."),
-                            )
-                          : ListView.builder(
-                              itemCount: Provider.of<SearchPageController>(context, listen: true).hits.length,
-                              itemBuilder: (BuildContext ctx, int index) {
-                                Map<String, dynamic> hit = Provider.of<SearchPageController>(context, listen: true).hits[index];
+                
+                // StreamBuilder<SearchResponse>(
+                //   stream:
+                //       Provider.of<SearchPageController>(context, listen: true)
+                //           .searcher
+                //           .responses, // 4. Listen and display search results!
+                //   builder: (BuildContext context, AsyncSnapshot<SearchResponse> snapshot) {
+                //     if (snapshot.hasData) {
+                //       final response = snapshot.data;
+                //       final hits = response?.hits.toList() ?? [];
+                //       return Expanded(child: ListView.builder(
+                //         itemCount: hits.length,
+                //         itemBuilder: (BuildContext context, int index) {
+                //           final hit = hits[index];
+                //           return ListTile(
+                //       title: RichText(
+                //         text: TextSpan(
+                //           style: Theme.of(context).textTheme.titleSmall,
+                //           children:
+                //               hit.getHighlightedString('username').toInlineSpans(),
+                //         ),
+                //       ),
+                //     );
+                //         },
+                //       ),);
+                //     } else {
+                //       return const CircularProgressIndicator();
+                //     }
+                //   },
+                // ),
 
-                                return TextButton(
-                                  onPressed: () {
-                                    print(hit["uid"]);
-                                  },
-                                  child: Text(hit["username"]),
-                                );
-                              },
-                            ),
-                ),
+                //Expanded(
+                //   child: Provider.of<SearchPageController>(context, listen: true).isLoading
+                //       ? const Center(
+                //           child: CircularProgressIndicator(),
+                //         )
+                //       : Provider.of<SearchPageController>(context, listen: true).hits.isEmpty
+                //           ? const Center(
+                //               child: Text("No results found."),
+                //             )
+                //           : ListView.builder(
+                //               itemCount: Provider.of<SearchPageController>(context, listen: true).hits.length,
+                //               itemBuilder: (BuildContext ctx, int index) {
+                //                 Map<String, dynamic> hit = Provider.of<SearchPageController>(context, listen: true).hits[index];
+
+                //                 return TextButton(
+                //                   onPressed: () {
+                //                     print(hit["uid"]);
+                //                   },
+                //                   child: Text(hit["username"]),
+                //                 );
+                //               },
+                //             ),
+                // ),
               ],
             ),
           ),
@@ -57,6 +92,3 @@ class SearchPage extends StatelessWidget {
     );
   }
 }
-
-
-
