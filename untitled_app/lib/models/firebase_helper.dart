2492d6c153a4b00e3e2e28b5011a7270a23e19ba
christianknab <christiantknab@gmail.com>
2023-10-29 18:04:00 -0700

    test

--- untitled_app/lib/models/firebase_helper.dart
+++ untitled_app/lib/models/firebase_helper.dart
@@ -46,17 +46,6 @@ class FirebaseHelper {
     await messaging.subscribeToTopic('new_post');
   }
 
-  static Future<void> testHealth() async {
-    HttpsCallable callable =
-        FirebaseFunctions.instance.httpsCallable('checkHealth');
-
-    final response = await callable.call();
-
-    if (response.data != null) {
-      debugPrint(response.data);
-    }
-  }
-
   static Future<void> _onBackgroundMessage(RemoteMessage message) async {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
@@ -64,30 +53,4 @@ class FirebaseHelper {
     debugPrint('we have received a notification ${message.notification}');
   }
 
-  static Future<bool> sendNotification({
-    required String title,
-    required String body,
-    required String token,
-    String? image,
-  }) async {
-    HttpsCallable callable =
-        FirebaseFunctions.instance.httpsCallable('sendNotification');
-
-    try {
-      final response = await callable.call(<String, dynamic>{
-        'title': title,
-        'body': body,
-        'image': image,
-        'token': token,
-      });
-
-      debugPrint('result is ${response.data ?? 'No data came back'}');
-
-      if (response.data == null) return false;
-      return true;
-    } catch (e) {
-      debugPrint('There was an error $e');
-      return false;
-    }
-  }
 }
