6eda8ef8a9eeafad9f81ef3e8898b4e7ac27a4c1
nunibye <38873297+nunibye@users.noreply.github.com>
2024-01-25 18:37:21 -0800

    fixed gray bar

--- untitled_app/lib/custom_widgets/gif_widget.dart
+++ untitled_app/lib/custom_widgets/gif_widget.dart
@@ -3,46 +3,79 @@ import 'package:untitled_app/localization/generated/app_localizations.dart';
 
 class GifWidget extends StatelessWidget {
   final String url;
-  const GifWidget({super.key, required this.url});
+  GifWidget({super.key, required this.url});
+  Future<ImageProvider> _loadImage(String url) async {
+    return NetworkImage(url);
+  }
 
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
-      child: Stack(
-        children: [
-          Container(
-            alignment: Alignment.center,
-            width: 200,
-            height: 150,
-            color: Theme.of(context).colorScheme.onBackground,
-            // child: CircularProgressIndicator(),
-          ),
-          Image.network(
-            url,
-            fit: BoxFit.fill,
-            errorBuilder: (context, error, stackTrace) =>
-                Text(AppLocalizations.of(context)!.gifLoadingError),
-            loadingBuilder: (BuildContext context, Widget child,
-                ImageChunkEvent? loadingProgress) {
-              if (loadingProgress == null) {
-                return child;
-              }
-              return Container(
-                alignment: Alignment.center,
-                width: 200,
-                height: 150,
-                child: CircularProgressIndicator(
-                  value: loadingProgress.expectedTotalBytes != null
-                      ? loadingProgress.cumulativeBytesLoaded /
-                          loadingProgress.expectedTotalBytes!
-                      : null,
-                ),
-              );
-            },
-          ),
-        ],
+      child: FutureBuilder<ImageProvider>(
+        future: _loadImage(url),
+        builder: (BuildContext context, AsyncSnapshot<ImageProvider> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Container(
+              alignment: Alignment.center,
+              width: 200,
+              height: 150,
+              child: const CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return Text(AppLocalizations.of(context)!.gifLoadingError);
+          } else {
+            return Image(
+              image: snapshot.data!,
+              fit: BoxFit.fill,
+            );
+          }
+        },
       ),
+      // Stack(
+      //   children: [
+      //     ValueListenableBuilder<bool>(
+      //       valueListenable: _isLoading,
+      //       builder: (context, isLoading, child) {
+      //         if (isLoading) {
+      //           return Container(
+      //             alignment: Alignment.center,
+      //             width: 200,
+      //             height: 150,
+      //             color: Theme.of(context).colorScheme.onBackground,
+      //           );
+      //         } else {
+      //           return const SizedBox.shrink(); // This will hide the container
+      //         }
+      //       },
+      //     ),
+      //     Image.network(
+      //       url,
+      //       fit: BoxFit.fill,
+      //       errorBuilder: (context, error, stackTrace) =>
+      //           Text(AppLocalizations.of(context)!.gifLoadingError),
+      //       loadingBuilder: (BuildContext context, Widget child,
+      //           ImageChunkEvent? loadingProgress) {
+
+      //         if (loadingProgress == null) {
+      //           _isLoading.value = false;
+      //           return child;
+      //         }
+      //         return Container(
+      //           alignment: Alignment.center,
+      //           width: 200,
+      //           height: 150,
+      //           child: CircularProgressIndicator(
+      //             value: loadingProgress.expectedTotalBytes != null
+      //                 ? loadingProgress.cumulativeBytesLoaded /
+      //                     loadingProgress.expectedTotalBytes!
+      //                 : null,
+      //           ),
+      //         );
+      //       },
+      //     ),
+      //   ],
+      // ),
     );
   }
 }
